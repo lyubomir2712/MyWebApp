18aaf7a41885c2681beca755f58958de1b060d6a
Lyubomir Georgiev <lyubomirgeorgiev2712@gmail.com>
2024-11-13 10:56:36 +0200

    added in program cs new route for areas

--- MyWebApp/MyWebApp.Presentation/Program.cs
+++ MyWebApp/MyWebApp.Presentation/Program.cs
@@ -50,6 +50,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
